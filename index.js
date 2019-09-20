43f924d91a111d5ea2916c630ae0285553df2285
Michael Wilson <v-miwils@microsoft.com>
2019-09-20 14:43:23 -0700

    added first portion of page accessibiliity modifier

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 (function() {
+  var VisualSegmentEnum = {
+    GENERAL: 0,
+    SUBTITLE: 1,
+    SECTION: 2,
+    PARAGRPAPH: 3
+  };
+
   if (document.readyState === "loading") {
     document.addEventListener("DOMContentLoaded", appendDefaults);
   } else {
@@ -16,5 +23,25 @@
 
   function getHomeComp() {
     $("main").load("./components/home/home.html");
+  }
+
+  /**
+   * Method to assist those with visual impairments
+   */
+  function adjustTextSize(visualSegmentEnum, fontSize, color) {
+    // detect internet explorer
+    let rootStylesheet = inIE()
+      ? document.styleSheets[0].rules[visualSegmentEnum]
+      : document.styleSheets[0].cssRules[visualSegmentEnum];
+    specificRule.style.fontSize = fontSize;
+    specificRule.style.color = color;
+  }
+
+  /**
+   * Checks to see if we are in internet explorer
+   */
+  function inIE() {
+    let userAgent = navigator.userAgent;
+    return userAgent.includes("MSIE") || userAgent.includes("Trident");
   }
 })();
